cf9be6dede886c210782147fe9656fec0b25e93a
Octavianus <oktagandajaya@gmail.com>
2020-10-15 22:39:46 +0700

    Set design sample file

--- XPack2/Themes/Colours/XPackColours.swift
+++ XPack2/Themes/Colours/XPackColours.swift
@@ -12,22 +12,5 @@ import UIKit
 // Tell your coders to only use colors from this set of variables
 class XPackColours {
     
-    static var primary: UIColor {
-        let r = CGFloat(248.0/255.0)
-        let g = CGFloat(146.0/255.0)
-        let b = CGFloat(1.0/255.0)
-        let a = CGFloat(1.0)
-        return UIColor(displayP3Red: r, green: g, blue: b, alpha: a)
-    }
-    
-    static var secondary: UIColor {
-        let r = CGFloat(71.0/255.0)
-        let g = CGFloat(95.0/255.0)
-        let b = CGFloat(55.0/255.0)
-        let a = CGFloat(1.0)
-        return UIColor(red: r, green: g, blue: b, alpha: a)
-    }
-    
-    static var lightGray = UIColor.lightGray.cgColor
-    
+
 }
